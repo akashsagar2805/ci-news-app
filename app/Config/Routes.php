12f6a94b4ca6d34c390d3dd182148bc0d24ffbe6
akashsagar2805 <akashsagar5869@gmail.com>
2024-12-16 23:42:47 +0530

    added all news and single news listing view and logic to get news records from db

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,11 +1,15 @@
 <?php
 
-use CodeIgniter\Router\RouteCollection;
+use App\Controllers\News;
 use App\Controllers\Pages;
+use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('news', [News::class, 'index']);           
+$routes->get('news/(:segment)', [News::class, 'show']);
+
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
